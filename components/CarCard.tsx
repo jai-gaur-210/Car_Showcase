fc7840a8198c033c9ed553c72511d85878f75cb4
jai <jaigaur.imp210@gmail.com>
2023-07-10 17:04:24 +0530

    commit

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -40,7 +40,7 @@ const CarCard = ({ car }: CarCardProps) => {
             </div>
 
             <div className='relative flex w-full mt-2'>
-                <div className='flex group-hover:invisible w-full justify-between text-grey'>
+                <div className='flex  w-full justify-between text-grey'>
                     <div className='flex flex-col justify-center items-center gap-2'>
                         <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
                         <p className='text-[14px] leading-[17px]'>
@@ -61,14 +61,7 @@ const CarCard = ({ car }: CarCardProps) => {
                     </div>
                 </div>
 
-                <div className='car-card__btn-container'>
-                    <CustomButton 
-                    title='View More'
-                    containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
-                    textStyle='text-white text-[14px] leading-[17px] font-bold'
-                    rightIcon='/right-arrow.svg'
-                    handleClick={() => setIsOpen(true)} />
-                </div>
+                
 
             </div>
 
